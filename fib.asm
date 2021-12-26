1b524e9ca99cecd68fe9b8255e596d67b90b340e
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-12-26 08:43:24 +0000

    upgraded to 256bit

--- fib.asm
+++ fib.asm
@@ -13,13 +13,13 @@
 	jp start
 	
 s1_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 s2_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 sumof_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 to_print_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 mainLoopCount_mem
 	DEFB 0
 mainCLSCountDown_mem
@@ -81,10 +81,38 @@ hprint128_loop
 	; restore registers
 	ld a, _NL
 	call PRINT
-	
 	ret
 	
-sum128 ;; add two 128bit values from memory locations s1 , then store in sumof
+hprint256  ; print one 32byte number stored in location $to_print
+	;ld hl,$to_print
+	ld hl,$to_print+31	
+	ld b,32	
+hprint256_loop	
+	ld a, (hl)
+	push af ;store the original value of a for later
+	and $f0 ; isolate the first digit
+	rra
+	rra
+	rra
+	rra
+	add a,$1c ; add 28 to the character code
+	call PRINT ;
+	pop af ; retrieve original value of a
+	and $0f ; isolate the second digit
+	add a,$1c ; add 28 to the character code
+	call PRINT
+	ld a, 00;_NL ;print new line ; 00 is space
+	;call PRINT ; print a space character
+	
+	dec hl
+	djnz hprint256_loop
+	; restore registers
+	ld a, _NL
+	call PRINT
+	
+	ret	
+	
+sum128 ;; add two 128bit values from memory locations s1,s2 , then store in sumof
 		;; all three locations MUST be consecutive in memory 16bytes (128bits!) appart
     ld hl,(s1) \ ld de,(s2) \ add hl,de \ ld (sumof),hl
     ld hl,(s1+2) \ ld de,(s2+2) \ adc hl,de \ ld (sumof+2),hl
@@ -95,6 +123,27 @@ sum128 ;; add two 128bit values from memory locations s1 , then store in sumof
 	ld hl,(s1+12) \ ld de,(s2+12) \ adc hl,de \ ld (sumof+12),hl
 	ld hl,(s1+14) \ ld de,(s2+14) \ adc hl,de \ ld (sumof+14),hl
 	ret
+
+sum256 ;; add two 256bit values from memory locations s1, s2, then store in sumof
+		;; all three locations MUST be consecutive in memory 16bytes (128bits!) appart
+    ld hl,(s1) \ ld de,(s2) \ add hl,de \ ld (sumof),hl
+    ld hl,(s1+2) \ ld de,(s2+2) \ adc hl,de \ ld (sumof+2),hl
+    ld hl,(s1+4) \ ld de,(s2+4) \ adc hl,de \ ld (sumof+4),hl
+    ld hl,(s1+6) \ ld de,(s2+6) \ adc hl,de \ ld (sumof+6),hl
+    ld hl,(s1+8) \ ld de,(s2+8) \ adc hl,de \ ld (sumof+8),hl
+	ld hl,(s1+10) \ ld de,(s2+10) \ adc hl,de \ ld (sumof+10),hl
+	ld hl,(s1+12) \ ld de,(s2+12) \ adc hl,de \ ld (sumof+12),hl
+	ld hl,(s1+14) \ ld de,(s2+14) \ adc hl,de \ ld (sumof+14),hl
+	
+	ld hl,(s1+16) \ ld de,(s2+16) \ adc hl,de \ ld (sumof+16),hl
+	ld hl,(s1+18) \ ld de,(s2+18) \ adc hl,de \ ld (sumof+18),hl
+	ld hl,(s1+20) \ ld de,(s2+20) \ adc hl,de \ ld (sumof+20),hl
+	ld hl,(s1+22) \ ld de,(s2+22) \ adc hl,de \ ld (sumof+22),hl
+	ld hl,(s1+24) \ ld de,(s2+24) \ adc hl,de \ ld (sumof+24),hl
+	ld hl,(s1+26) \ ld de,(s2+26) \ adc hl,de \ ld (sumof+26),hl
+	ld hl,(s1+28) \ ld de,(s2+28) \ adc hl,de \ ld (sumof+28),hl
+	ld hl,(s1+30) \ ld de,(s2+30) \ adc hl,de \ ld (sumof+30),hl	
+	ret	
 	
 zero128				; zero 128bits starting from hl,
 					; b and hl clobbered by this
@@ -104,39 +153,48 @@ zero128_loop
 	inc hl	
 	djnz zero128_loop		; jump if loop control variable not zero
 	ret
+
+zero256				; zero 128bits starting from hl,
+					; b and hl clobbered by this
+	ld b,$10			; set loop control variable for next loop "$" shows its hex
+zero256_loop
+	ld (hl),0			; set the thing hl points to to a constant
+	inc hl	
+	djnz zero256_loop		; jump if loop control variable not zero
+	ret	
 	
 start
 
 	call CLS	
 
 	ld hl, s1		;;; initialise s1 s2 and sumof all to zero
-	call zero128	
+	call zero256	
 	ld hl, s2	
-	call zero128		
+	call zero256		
 	ld hl, sumof		
-	call zero128
+	call zero256
 					;;; set s2 to the second in fibonacci sequence ie "1", s1 is already zero	
 	ld hl, s2		
 	ld (hl),1	
 
 	ld hl, s1		; copy s1 to print buffer 
 	ld de, to_print		;
-	ld bc, 16
+	ld bc, 32
 	ldir				; copy 16 bytes from hl to de	
-	call hprint128 		; print a 128 bit (16byte numnber as hex to screen)
+	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
 
 	ld hl, s2		; copy s2 to print buffer 
 	ld de, to_print		;
-	ld bc, 16
+	ld bc, 32
 	ldir				; copy 16 bytes from hl to de	
-	call hprint128 		; print a 128 bit (16byte numnber as hex to screen)
+	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
 	
 	ld hl,mainLoopCount
-	ld a,$14
+	ld a,8
 	ld (hl),a
 
 	ld hl,mainCLSCountDown_mem  ;; limit the number of screen clears and the 23 * 8 fib numbers
-	ld a,9
+	ld a,36
 	ld (hl),a
 		
 	
@@ -145,23 +203,23 @@ mainloopFib
 	; s1 contains first number to add
 	; s2 contains second number to add
 	; sumof is result	
-	call sum128
+	call sum256
 
 	ld hl, sumof		; copy sumof to print buffer 
 	ld de, to_print		;
-	ld bc, 16
+	ld bc, 32
 	ldir				; move 16 bytes from hl of de
 
-	call hprint128 		; print a 128 bit (16byte numnber as hex to screen)
+	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
 	
 	ld hl, s2			; copy s2 to s1 
 	ld de, s1			;
-	ld bc, 16
+	ld bc, 32
 	ldir				; move 16 bytes from hl of de
 	
 	ld hl, sumof		; copy sumof to s2
 	ld de, s2			;
-	ld bc, 16
+	ld bc, 32
 	ldir				; move 16 bytes from hl of de
 
 	ld hl,mainLoopCount
@@ -172,7 +230,7 @@ mainloopFib
 	ld (hl),a
 
 	ld hl,mainLoopCount
-	ld a,$14
+	ld a,10
 	ld (hl),a	
 
 	ld hl,mainCLSCountDown_mem
@@ -180,7 +238,7 @@ mainloopFib
 	dec a
 	jp z, endPROG
 	ld (hl),a
-	
+	call hprint 	; print out how many iterations (of screen clears)
 	call CLS	
 	
 	jp mainloopFib
@@ -188,6 +246,7 @@ mainloopFib
 skip
 	ld hl,mainLoopCount
 	ld (hl),a			; save a back to (hl) (mainLoopCount)
+	
 	jp mainloopFib	
 	
 endPROG
