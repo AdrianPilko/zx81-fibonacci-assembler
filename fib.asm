6f8434970a3e05f98c29000f1121e0150186d416
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-12-27 08:41:12 +0000

    updated to 512bit

--- fib.asm
+++ fib.asm
@@ -15,13 +15,13 @@
 	jp start
 	
 s1_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 s2_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 sumof_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 to_print_mem
-	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DEFB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 mainLoopCount_mem
 	DEFB 0
 mainCLSCountDown_mem
@@ -108,6 +108,36 @@ hprint256_loop
 	call PRINT
 	
 	ret	
+
+hprint512  ; print one 64byte number stored in location $to_print
+		   ; this takes 8 lines on zx81 screen
+	ld hl,$to_print+63
+	ld b,64	
+hprint512_loop	
+	ld a, (hl)
+	push af ;store the original value of a for later
+	and $f0 ; isolate the first digit
+	rra
+	rra
+	rra
+	rra
+	add a,$1c ; add 28 to the character code
+	call PRINT ;
+	pop af ; retrieve original value of a
+	and $0f ; isolate the second digit
+	add a,$1c ; add 28 to the character code
+	call PRINT
+	ld a, 00;_NL ;print new line ; 00 is space
+	;call PRINT ; print a space character
+	
+	dec hl
+	djnz hprint512_loop
+	; restore registers
+	ld a, _NL
+	call PRINT
+	ld a, _NL
+	call PRINT	
+	ret		
 	
 sum128 ;; add two 128bit values from memory locations s1,s2 , then store in sumof
 		;; all three locations MUST be consecutive in memory 16bytes (128bits!) appart
@@ -142,6 +172,45 @@ sum256 ;; add two 256bit values from memory locations s1, s2, then store in sumo
 	ld hl,(s1+30) \ ld de,(s2+30) \ adc hl,de \ ld (sumof+30),hl	
 	ret	
 	
+sum512 ;; add two 512bit values from memory locations s1, s2, then store in sumof
+		;; all three locations MUST be consecutive in memory 16bytes (128bits!) appart
+    ld hl,(s1) \ ld de,(s2) \ add hl,de \ ld (sumof),hl
+    ld hl,(s1+2) \ ld de,(s2+2) \ adc hl,de \ ld (sumof+2),hl
+    ld hl,(s1+4) \ ld de,(s2+4) \ adc hl,de \ ld (sumof+4),hl
+    ld hl,(s1+6) \ ld de,(s2+6) \ adc hl,de \ ld (sumof+6),hl
+    ld hl,(s1+8) \ ld de,(s2+8) \ adc hl,de \ ld (sumof+8),hl
+	ld hl,(s1+10) \ ld de,(s2+10) \ adc hl,de \ ld (sumof+10),hl
+	ld hl,(s1+12) \ ld de,(s2+12) \ adc hl,de \ ld (sumof+12),hl
+	ld hl,(s1+14) \ ld de,(s2+14) \ adc hl,de \ ld (sumof+14),hl
+	
+	ld hl,(s1+16) \ ld de,(s2+16) \ adc hl,de \ ld (sumof+16),hl
+	ld hl,(s1+18) \ ld de,(s2+18) \ adc hl,de \ ld (sumof+18),hl
+	ld hl,(s1+20) \ ld de,(s2+20) \ adc hl,de \ ld (sumof+20),hl
+	ld hl,(s1+22) \ ld de,(s2+22) \ adc hl,de \ ld (sumof+22),hl
+	ld hl,(s1+24) \ ld de,(s2+24) \ adc hl,de \ ld (sumof+24),hl
+	ld hl,(s1+26) \ ld de,(s2+26) \ adc hl,de \ ld (sumof+26),hl
+	ld hl,(s1+28) \ ld de,(s2+28) \ adc hl,de \ ld (sumof+28),hl
+	ld hl,(s1+30) \ ld de,(s2+30) \ adc hl,de \ ld (sumof+30),hl	
+
+	ld hl,(s1+32) \ ld de,(s2+32) \ adc hl,de \ ld (sumof+32),hl
+	ld hl,(s1+34) \ ld de,(s2+34) \ adc hl,de \ ld (sumof+34),hl
+	ld hl,(s1+36) \ ld de,(s2+36) \ adc hl,de \ ld (sumof+36),hl
+	ld hl,(s1+38) \ ld de,(s2+38) \ adc hl,de \ ld (sumof+38),hl
+	ld hl,(s1+40) \ ld de,(s2+40) \ adc hl,de \ ld (sumof+40),hl
+	ld hl,(s1+42) \ ld de,(s2+42) \ adc hl,de \ ld (sumof+42),hl
+	ld hl,(s1+44) \ ld de,(s2+44) \ adc hl,de \ ld (sumof+44),hl
+	ld hl,(s1+46) \ ld de,(s2+46) \ adc hl,de \ ld (sumof+46),hl	
+
+	ld hl,(s1+48) \ ld de,(s2+48) \ adc hl,de \ ld (sumof+48),hl
+	ld hl,(s1+50) \ ld de,(s2+50) \ adc hl,de \ ld (sumof+50),hl
+	ld hl,(s1+52) \ ld de,(s2+52) \ adc hl,de \ ld (sumof+52),hl
+	ld hl,(s1+54) \ ld de,(s2+54) \ adc hl,de \ ld (sumof+54),hl
+	ld hl,(s1+56) \ ld de,(s2+56) \ adc hl,de \ ld (sumof+56),hl
+	ld hl,(s1+58) \ ld de,(s2+58) \ adc hl,de \ ld (sumof+58),hl
+	ld hl,(s1+60) \ ld de,(s2+60) \ adc hl,de \ ld (sumof+60),hl
+	ld hl,(s1+62) \ ld de,(s2+62) \ adc hl,de \ ld (sumof+62),hl		
+	ret		
+	
 zero128				; zero 128bits starting from hl,
 					; b and hl clobbered by this
 	ld b,$10			; set loop control variable for next loop "$" shows its hex
@@ -151,7 +220,7 @@ zero128_loop
 	djnz zero128_loop		; jump if loop control variable not zero
 	ret
 
-zero256				; zero 128bits starting from hl,
+zero256				; zero 256bits starting from hl,
 					; b and hl clobbered by this
 	ld b,$10			; set loop control variable for next loop "$" shows its hex
 zero256_loop
@@ -160,38 +229,47 @@ zero256_loop
 	djnz zero256_loop		; jump if loop control variable not zero
 	ret	
 	
+zero512				; zero 512bits starting from hl,
+					; b and hl clobbered by this
+	ld b,$10			; set loop control variable for next loop "$" shows its hex
+zero512_loop
+	ld (hl),0			; set the thing hl points to to a constant
+	inc hl	
+	djnz zero512_loop		; jump if loop control variable not zero
+	ret		
+	
 start
 
 	call CLS	
 
 	ld hl, s1		;;; initialise s1 s2 and sumof all to zero
-	call zero256	
+	call zero512	
 	ld hl, s2	
-	call zero256		
+	call zero512
 	ld hl, sumof		
-	call zero256
+	call zero512
 					;;; set s2 to the second in fibonacci sequence ie "1", s1 is already zero	
 	ld hl, s2		
 	ld (hl),1	
 
 	ld hl, s1		; copy s1 to print buffer 
 	ld de, to_print		;
-	ld bc, 32
-	ldir				; copy 16 bytes from hl to de	
-	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
+	ld bc, 64
+	ldir				; copy 64 bytes from hl to de	
+	call hprint512 		; print a 512 bit (64byte numnber as hex to screen)
 
 	ld hl, s2		; copy s2 to print buffer 
 	ld de, to_print		;
-	ld bc, 32
-	ldir				; copy 16 bytes from hl to de	
-	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
+	ld bc, 64
+	ldir				; copy 64 bytes from hl to de	
+	call hprint512 		; print a 512 bit (64byte numnber as hex to screen)
 	
-	ld hl,mainLoopCount
-	ld a,8
+	ld hl,mainLoopCount  ; can only display 2 more 64byte numbers on screen on first output (prev hprint calls did 2 here)
+	ld a,2
 	ld (hl),a
 
-	ld hl,mainCLSCountDown_mem  ;; limit the number of screen clears and the 23 * 8 fib numbers
-	ld a,36
+	ld hl,mainCLSCountDown_mem  ;; limit the number of screen clears and the 183 * 4 fib numbers
+	ld a,183
 	ld (hl),a
 		
 	
@@ -200,24 +278,24 @@ mainloopFib
 	; s1 contains first number to add
 	; s2 contains second number to add
 	; sumof is result	
-	call sum256
+	call sum512
 
 	ld hl, sumof		; copy sumof to print buffer 
 	ld de, to_print		;
-	ld bc, 32
-	ldir				; move 16 bytes from hl of de
+	ld bc, 64
+	ldir				; move 64 bytes from hl of de
 
-	call hprint256 		; print a 128 bit (16byte numnber as hex to screen)
+	call hprint512 		; print a 512 bit (16byte numnber as hex to screen)
 	
 	ld hl, s2			; copy s2 to s1 
 	ld de, s1			;
-	ld bc, 32
-	ldir				; move 16 bytes from hl of de
+	ld bc, 64
+	ldir				; move 64 bytes from hl of de
 	
 	ld hl, sumof		; copy sumof to s2
 	ld de, s2			;
-	ld bc, 32
-	ldir				; move 16 bytes from hl of de
+	ld bc, 64
+	ldir				; move 64 bytes from hl of de
 
 	ld hl,mainLoopCount
 	ld a,(hl)	
@@ -227,7 +305,7 @@ mainloopFib
 	ld (hl),a
 
 	ld hl,mainLoopCount
-	ld a,10
+	ld a,4
 	ld (hl),a	
 
 	ld hl,mainCLSCountDown_mem
